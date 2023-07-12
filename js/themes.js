b2e28694b2f48b21095802692d2841b89beb2a06
Alexander <79362302+E314-byte@users.noreply.github.com>
2023-07-12 19:46:14 +0300

    2.2

--- js/themes.js
+++ js/themes.js
@@ -6,43 +6,65 @@ const input = document.querySelector('.input-short');
 const rang = document.querySelector('.styled-slider');
 
 const checkbox = document.querySelectorAll('.checkbox');
+const radio = document.querySelectorAll('.title-lite');
+console.log(radio);
 
 
 const themes = document.querySelector('.themes');
-const themesImg = document.querySelector('.themes span ');
+// const themesImg = document.querySelector('.themes span');
 
 
-themes.addEventListener('click', function () {
-    // надо по-умному сделать (изменение картинки в кнопке)
-    themes.classList.toggle('themes--dark');
+themes.addEventListener('click', function (item) {
+    //кастыль 
+    if (themes.classList.toggle('themes--dark')) {
 
+        body.classList.toggle('dark-theme');
+        price.classList.toggle('dark-theme');
+        input.classList.toggle('input-short--dark');
+        // слайдер 
+        rang.classList.toggle('styled-slider');
 
+        rooms.forEach((item) => {
+            item.classList.toggle('rooms-radio-fake--dark');
+        });
+        calcSection.forEach((item) => {
+            item.classList.toggle('calc-section--dark');
+        });
 
-    body.classList.toggle('dark-theme');
-    price.classList.toggle('dark-theme');
-    input.classList.toggle('input-short--dark');
-    // слайдер 
-    rang.classList.toggle('styled-slider');
+        radio.forEach((item) => {
+            var color = "#6A78A0"
+            item.style.setProperty('--bc-color', color);
+            var color1 = "#fff"
+            item.style.setProperty('--bc-color1', color1)
+        })
+    } else {
+        body.classList.toggle('dark-theme');
+        price.classList.toggle('dark-theme');
+        input.classList.toggle('input-short--dark');
+        // слайдер 
+        rang.classList.toggle('styled-slider');
 
-    rooms.forEach((item) => {
-        item.classList.toggle('rooms-radio-fake--dark');
-    });
-    calcSection.forEach((item) => {
-        item.classList.toggle('calc-section--dark');
-    });
-
-
-
-    // checkbox.forEach((item) => {
-    //     item.style.border = 'blue';
-    //     console.log(checkbox);
-
-    // });
-
+        rooms.forEach((item) => {
+            item.classList.toggle('rooms-radio-fake--dark');
+        });
+        calcSection.forEach((item) => {
+            item.classList.toggle('calc-section--dark');
+        });
+        radio.forEach((item) => {
+            var color = ""
+            item.style.setProperty('--bc-color', color);
+            var color1 = ""
+            item.style.setProperty('--bc-color1', color1)
+        })
+    }
 });
 
-// function changeImage() {
-//     let button = document.getElementById("myButton");
-//     button.src = "../img/icon/sun.png";
-// }
 
+// themes.querySelector(".themes").addEventListener('click', function () {
+//     this.forEach((item) => {
+//         var color = "#6A78A0"
+//         item.querySelectorAll(".title-lite").style.setProperty('--bc-color', color);
+//         var color1 = "#fff"
+//         item.querySelectorAll(".title-lite").style.setProperty('--bc-color1', color1)
+//     })
+// })
